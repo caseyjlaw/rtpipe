b788232f244c20d7739139780155394da5d7583a
Casey Law <caseyjlaw@gmail.com>
2015-07-28 16:37:09 -0600

    using build system and .gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'rtpipe',
+    description = 'Python scripts for fast transient searches with radio interferometer data',
+    author = 'Casey Law',
+    author_email = 'caseyjlaw@gmail.com',
+    version = '0.0',
+    packages = find_packages(),        # get all python scripts in realtime
+    dependency_links = ['http://github.com/caseyjlaw/sdmpy', 'http://github.com/caseyjlaw/sdmreader']
+)
