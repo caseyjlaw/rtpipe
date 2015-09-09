98554eb0c0025c8e3e1b02f9e583ec55fa986629
Casey Law <caseyjlaw@gmail.com>
2015-09-09 13:52:38 -0600

    changed and to &

--- rtpipe/parsecal.py
+++ rtpipe/parsecal.py
@@ -412,7 +412,7 @@ class telcal_sol():
         """
 
         # identify very low gain amps not already flagged
-        badsols = n.where( (n.median(self.amp)/self.amp > threshold) and (self.flagged == False))[0]
+        badsols = n.where( (n.median(self.amp)/self.amp > threshold) & (self.flagged == False))[0]
         if len(badsols):
             self.logger.info('Solutions %s flagged (times %s, ants %s, freqs %s) for low gain amplitude.' % (str(badsols), self.mjd[badsols], self.antname[badsols], self.ifid[badsols]))
             for sol in n.where(n.median(self.amp)/self.amp > 50)[0]: 
