5157fb1dfe659f4cb401dcd427248b172218bfdb
Casey Law <caseyjlaw@gmail.com>
2015-11-06 15:50:03 -0700

    autoscaled snr, hbox/vbox layout, normprob, mostly ready for server filtering

--- rtpipe/candvis.py
+++ rtpipe/candvis.py
@@ -1,14 +1,16 @@
 from scipy.special import erfinv
 import numpy as n
-import logging
+import logging, pickle
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 from bokeh.plotting import ColumnDataSource, figure, save, output_file, vplot
+from bokeh.models.widgets import VBox, HBox
 from bokeh.models import HoverTool
 from collections import OrderedDict 
 
-def plot_interactive(mergepkl, thresh=6.8, snrbase=5.5):
+def plot_interactive(mergepkl, thresh=6.0, snrbase=5.5, savehtml=True):
     """ Make interactive summary plot with bokeh
+    if savehtml will write to html, otherwise returns tuple of bokeh plot objects
     """
 
     with open(mergepkl,'r') as pkl:
@@ -21,50 +23,67 @@ def plot_interactive(mergepkl, thresh=6.8, snrbase=5.5):
     # get columns
     snrorig = [cands[k][0] for k in cands.iterkeys()]
     
-    snr,dm,l1,m1,time,specstd,imkur,key,sizes,colors = candfilter(d, cands, 0)
-    dm_min = min(dm); dm_max = max(dm)
+    snr,dm,l1,m1,time,specstd,imkur,key,sizes,colors,zs = candfilter(d, cands, thresh=0)
+    dm_min = min(min(dm), max(dm)/1.2); dm_max = max(max(dm), min(dm)*1.2)
     time_min = min(time); time_max = max(time)
     specstd_min = min(specstd); specstd_max = max(specstd)
     imkur_min = min(imkur); imkur_max = max(imkur)
     l1_min = min(l1); l1_max = max(l1)
     m1_min = min(m1); m1_max = max(m1)
 
-    zspos, zsneg = normprob(d, snrs)
-    snr,dm,l1,m1,time,specstd,imkur,key,sizes,colors = candfilter(d, cands, thresh)
-    source = ColumnDataSource(data=dict(snr=snr, dm=dm, l1=l1, m1=m1, time=time, specstd=specstd, imkur=imkur, key=key, sizes=sizes, colors=colors, zs=zspos))
-    snr,dm,l1,m1,time,specstd,imkur,key,sizes,colors = candfilter(d, cands, -1*thresh)
-    sourceneg = ColumnDataSource(data=dict(snr=snr, dm=dm, l1=l1, m1=m1, time=time, specstd=specstd, imkur=imkur, key=key, sizes=sizes, colors=colors, zs=zsneg))
+    snr,dm,l1,m1,time,specstd,imkur,key,sizes,colors,zs = candfilter(d, cands, thresh)
+    source = ColumnDataSource(data=dict(snr=snr, dm=dm, l1=l1, m1=m1, time=time, specstd=specstd, imkur=imkur, key=key, sizes=sizes, colors=colors, zs=zs))
+    snr,dm,l1,m1,time,specstd,imkur,key,sizes,colors,zs = candfilter(d, cands, -1*thresh)
+    sourceneg = ColumnDataSource(data=dict(snr=snr, dm=dm, l1=l1, m1=m1, time=time, specstd=specstd, imkur=imkur, key=key, sizes=sizes, colors=colors, zs=zs, abssnr=n.abs(snr)))
 
     # DM-time plot
     dmt = figure(plot_width=900, plot_height=400, toolbar_location="left", x_axis_label='Time (s; rough)', y_axis_label='DM (pc/cm3)', x_range=(time_min, time_max), y_range=(dm_min, dm_max), webgl=True, tools=TOOLS)
     dmt.circle('time', 'dm', size='sizes', source=source, line_color=None, fill_color='colors', fill_alpha=0.3)
-    dmt.cross('time', 'dm', size=10, source=sourceneg, line_color='colors', line_alpha=0.3)
+    dmt.cross('time', 'dm', size='sizes', source=sourceneg, line_color='colors', line_alpha=0.3)
 
+    # image location plot
     loc = figure(plot_width=450, plot_height=400, toolbar_location="left", x_axis_label='l1 (rad)', y_axis_label='m1 (rad)', x_range=(l1_min, l1_max), y_range=(m1_min,m1_max), tools=TOOLS, webgl=True)
     loc.circle('l1', 'm1', size='sizes', source=source, line_color=None, fill_color='colors', fill_alpha=0.3)
-    loc.cross('l1', 'm1', size=10, source=sourceneg, line_color='colors', line_alpha=0.3)
+    loc.cross('l1', 'm1', size='sizes', source=sourceneg, line_color='colors', line_alpha=0.3)
 
+    # cand spectrum/image statistics plot
     stat = figure(plot_width=450, plot_height=400, toolbar_location="left", x_axis_label='Spectral std', y_axis_label='Image kurtosis', x_range=(specstd_min, specstd_max), y_range=(imkur_min, imkur_max), tools=TOOLS, webgl=True)
     stat.circle('specstd', 'imkur', size='sizes', source=source, line_color=None, fill_color='colors', fill_alpha=0.3)
-    stat.cross('specstd', 'imkur', size=10, source=sourceneg, line_color='colors', line_alpha=0.3)
+    stat.cross('specstd', 'imkur', size='sizes', source=sourceneg, line_color='colors', line_alpha=0.3)
     
-    norm = figure(plot_width=450, plot_height=400, toolbar_location="left", x_axis_label='SNR observed', y_axis_label='SNR expected', tools=TOOLS, webgl=True)
-    stat.circle('snr', 'zs', size='sizes', source=source, line_color=None, fill_color='colors', fill_alpha=0.3)
-    stat.cross('snr', 'zs', size=10, source=sourceneg, line_color='colors', line_alpha=0.3)
+    # norm prob plot
+    norm = figure(plot_width=400, plot_height=400, toolbar_location="left", x_axis_label='SNR observed', y_axis_label='SNR expected', tools=TOOLS, webgl=True)
+    norm.circle('snr', 'zs', size='sizes', source=source, line_color=None, fill_color='colors', fill_alpha=0.3)
+    norm.cross('abssnr', 'zs', size='sizes', source=sourceneg, line_color='colors', line_alpha=0.3)
     
     hover = dmt.select(dict(type=HoverTool))
-    hover.tooltips = OrderedDict([('key', '@key'), ('time', '@time')])
+    hover.tooltips = OrderedDict([('SNR', '@snr'), ('time', '@time'), ('key', '@key')])
     hover = loc.select(dict(type=HoverTool))
-    hover.tooltips = OrderedDict([('key', '@key'), ('time', '@time')])
+    hover.tooltips = OrderedDict([('SNR', '@snr'), ('time', '@time'), ('key', '@key')])
     hover = stat.select(dict(type=HoverTool))
-    hover.tooltips = OrderedDict([('key', '@key'), ('time', '@time')])
+    hover.tooltips = OrderedDict([('SNR', '@snr'), ('time', '@time'), ('key', '@key')])
+    hover = norm.select(dict(type=HoverTool))
+    hover.tooltips = OrderedDict([('SNR', '@snr'), ('time', '@time'), ('key', '@key')])
 
-    save(vplot(dmt, loc, stat))
+    # arrange plots
+    top = HBox(children=[dmt])
+    middle = HBox(children=[loc, stat])
+    bottom = HBox(children=[norm])
+    combined = VBox(children=[top,middle,bottom])
+
+    if savehtml:
+        save(combined)
+    else:
+        return combined
 
 def normprob(d, snrs):
     """ Function takes state dict and snr list 
     Returns list of expected snr given each input value's frequency of occurrence via the normal probability assumption
+    input should be all of one sign (all pos or neg)
     """
+
+    signs = [n.sign(snr) for snr in snrs]
+    assert all(signs) or not all(signs), 'Signs of all snr values should be the same'
 
     # define norm quantile functions
     Z = lambda quan: n.sqrt(2)*erfinv( 2*quan - 1) 
@@ -82,20 +101,23 @@ def normprob(d, snrs):
     logger.info('Calculating normal probability distribution for npix*nints*ndms*dtfactor = %d' % (ntrials))
 
     # calc normal quantile
-    snrsortpos = sorted([s for s in snrs if s > 0], reverse=True)
-    if snrsortpos:
-        zvalpos = [Z(quan(ntrials, snrsortpos.index(snr)+1)) for snr in snrs if snr > 0]   # assumes unique snr values
-    snrsortneg = sorted([abs(s) for s in snrs if s < 0], reverse=True)
-    if snrsortneg:
-        zvalneg = [Z(quan(ntrials, snrsortneg.index(abs(snr))+1)) for snr in snrs if snr < 0]
+    try:
+        if n.sign(snrs[0]) > 0:
+            snrsort = sorted([s for s in snrs if s > 0], reverse=True)
+            zval = [Z(quan(ntrials, snrsort.index(snr)+1)) for snr in snrs]   # assumes unique snr values
+        elif n.sign(snrs[0]) < 0:
+            snrsort = sorted([abs(s) for s in snrs if s < 0], reverse=True)
+            zval = [Z(quan(ntrials, snrsort.index(abs(snr))+1)) for snr in snrs if snr < 0]
+    except IndexError, ValueError:
+        zval = []
 
-    return zvalpos, zvalneg
+    return zval
 
 def candfilter(d, cands, thresh=0):
     """ filters candidate data. if thresh is negative, returns values less than thresh.
     default thresh=0 returns all
     """
-    
+
     if thresh > 0:
         snr = [cands[k][0] for k in cands.iterkeys() if cands[k][0] > thresh]
         dm = [d['dmarr'][k[3]] for k in cands.iterkeys() if cands[k][0] > thresh]
@@ -105,7 +127,7 @@ def candfilter(d, cands, thresh=0):
         specstd = [cands[k][4] for k in cands.iterkeys() if cands[k][0] > thresh]
         imkur = [cands[k][8] for k in cands.iterkeys() if cands[k][0] > thresh]
         key = [k for k in cands.iterkeys() if cands[k][0] > thresh]
-        sizes = [(s - snrbase)**4 for s in snr]
+        zs = normprob(d, snr)
     elif thresh < 0:
         snr = [cands[k][0] for k in cands.iterkeys() if cands[k][0] < thresh]
         dm = [d['dmarr'][k[3]] for k in cands.iterkeys() if cands[k][0] < thresh]
@@ -115,7 +137,7 @@ def candfilter(d, cands, thresh=0):
         specstd = [cands[k][4] for k in cands.iterkeys() if cands[k][0] < thresh]
         imkur = [cands[k][8] for k in cands.iterkeys() if cands[k][0] < thresh]
         key = [k for k in cands.iterkeys() if cands[k][0] < thresh]
-        sizes = [(s - snrbase)**4 for s in snr]
+        zs = normprob(d, snr)
     else:
         snr = [cands[k][0] for k in cands.iterkeys()]
         dm = [d['dmarr'][k[3]] for k in cands.iterkeys()]
@@ -125,11 +147,21 @@ def candfilter(d, cands, thresh=0):
         specstd = [cands[k][4] for k in cands.iterkeys()]
         imkur = [cands[k][8] for k in cands.iterkeys()]
         key = [k for k in cands.iterkeys()]
-        sizes = [(s - snrbase)**4 for s in snr]
+        zs = []
 
-    colors = colorsat(l1,m1)
-    return snr,dm,l1,m1,time,specstd,imkur,key,sizes,colors
+    sizes = calcsize(snr)
+    colors = colorsat(l1, m1)
+    return snr, dm, l1, m1, time, specstd, imkur, key, sizes, colors, zs
 
+def calcsize(snr, sizerange=(5,50)):
+    smax = max([abs(s) for s in snr])
+    smin = min([abs(s) for s in snr])
+
+    if snr:
+        return [sizerange[0] + sizerange[1] * ((abs(s) - smin)/(smax - smin))**4 for s in snr]
+    else:
+        return []
+    
 def colorsat(l,m):
     """ Returns color for given l,m
     Designed to look like a color wheel that is more saturated in middle.
