9db0948619ab800b5779f35ed174ee9eec805ae2
Casey Law <caseyjlaw@gmail.com>
2016-01-08 16:58:37 -0700

    added dependency and bumped version

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ setup(
     description = 'Python scripts for fast transient searches with radio interferometer data',
     author = 'Casey Law',
     author_email = 'caseyjlaw@gmail.com',
-    version = '1.26',
+    version = '1.27',
     url = 'http://github.com/caseyjlaw/rtpipe',
     packages = find_packages(),        # get all python scripts in realtime
     dependency_links = ['http://github.com/caseyjlaw/sdmpy', 'http://github.com/caseyjlaw/sdmreader'],
-    install_requires=['cython', 'smdpy', 'sdmreader', 'scipy', 'numpy', 'pwkit'],
+    install_requires=['cython', 'smdpy', 'sdmreader', 'scipy', 'numpy', 'pwkit', 'pyfftw'],
     cmdclass = {'build_ext': build_ext},
     ext_modules = ext_modules
 )
