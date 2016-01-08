3eaa5068085985af367e9713e6edbb5ef1375a32
Casey Law <caseyjlaw@gmail.com>
2016-01-08 16:51:34 -0700

    added dependencies for building

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     url = 'http://github.com/caseyjlaw/rtpipe',
     packages = find_packages(),        # get all python scripts in realtime
     dependency_links = ['http://github.com/caseyjlaw/sdmpy', 'http://github.com/caseyjlaw/sdmreader'],
-    install_requires=['cython'],
+    install_requires=['cython', 'smdpy', 'sdmreader', 'scipy', 'numpy', 'pwkit'],
     cmdclass = {'build_ext': build_ext},
     ext_modules = ext_modules
 )
