75c03cd97a363ca003449b345a7b724e47e27ff7
Casey Law <caseyjlaw@gmail.com>
2016-03-17 17:43:24 -0600

    sketch of a git-tracked classification pkl system

--- rtpipe/classification.py
+++ rtpipe/classification.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import Imputer
 from rtpipe.parsecands import read_candidates
 from pickle_utils import *
 from sklearn_utils import *
+from sh import git, mv
 
 logging.basicConfig(level=logging.INFO)
 
@@ -25,29 +26,13 @@ def load_classifier(clf_file):
     return clf
 
 
-def classify(feats, rbversion, njobs=1, verbose=0):
+def classify(feats, rb_version, njobs=1, verbose=0):
 
     # validate RB version
-    validate_rb_version(rbversion)
-
-    # # impute any missing values
-    # #
-    # imp = Imputer(missing_values='NaN', strategy='mean', axis=0) # UGH, 'median' has a bug in 0.14
-    # imp.fit(feats)
-    # dat = imp.transform(feats)
-
-    # load pickled classifier
-    #
-    this_dir = os.path.split(os.path.abspath(__file__))[0]
-    f_clfpkl = os.path.join(this_dir,"classify_ET.%s.pkl" % rbversion)
-
-    if not os.path.isfile(f_clfpkl):
-        raise IOError("classifier pkl file %s not found" % f_clfpkl)
-    logging.info("f_clfpkl=%s" % f_clfpkl)
-
+    validate_rb_version(rb_version)
+    f_clfpkl = get_rb_pkl(rb_version)
 
     # load classifier and update classifier parameters according to user input
-    #
     try:
         clf = load_classifier(f_clfpkl)
         clf.n_jobs  = njobs
@@ -93,6 +78,37 @@ def validate_rb_version(rb_version):
         raise ValueError("Invalid training version specified: %s" %  rb_version)
     if feat_version < 1 or feat_version > MAX_FEAT_VERSION:
         raise ValueError("Invalid feature version specified: %s" % rb_version)
+
+
+def get_rb_pkl(rb_version, mode='old', repopath='.'):
+
+    # load pickled classifier
+
+    if mode == 'old':
+        this_dir = os.path.split(os.path.abspath(__file__))[0]
+        f_clfpkl = os.path.join(this_dir,"classify_ET.%s.pkl" % rb_version)
+    elif mode == 'new':
+        repo = git.bake(_cwd=repopath)
+        repo.checkout(rb_version)
+        f_clfpkl = os.path.join(repopath, 'classify_ET.pkl')  # fixed name?
+    else:
+        raise IOError("mode {0} not known".format(mode))
+
+    if not os.path.isfile(f_clfpkl):
+        raise IOError("classifier pkl file %s not found" % f_clfpkl)
+    logging.info("f_clfpkl=%s" % f_clfpkl)
+
+    return f_clfpkl
+
+
+def update_rb_repo(f_clfpkl, rb_version, repopath='.', commitmsg=''):
+
+    repo = git.bake(_cwd=repopath)
+    mv(f_clfpkl, os.path.join(repopath, f_clfpkl))
+    repo.add(f_clfpkl)
+    repo.commit(m=commitmsg)
+    repo.tag(rb_version)
+    repo.push()
 
 
 def lookup_features(rb_version):
