8c91b9578603fb9d452c95632301f8c457bbddfb
Casey Law <caseyjlaw@gmail.com>
2016-05-19 16:00:07 -0600

    making nbpipeline class based to keep track of state directory

--- rtpipe/nbpipeline.py
+++ rtpipe/nbpipeline.py
@@ -1,101 +1,96 @@
-from ipywidgets import interact, FloatSlider, Text, Dropdown, Button, Output, VBox, fixed
+from ipywidgets import interact, FloatSlider, Text, Dropdown, Output, VBox, fixed
 import pickle, os
 from IPython.display import display, Javascript
 
-
-def save(obj, label, format='text', statedir='.nbpipeline'):
-    """ Save or update obj as pkl file with name label 
-
-    format can be 'text' or 'pickle'.
-    statedir is the directory name for saving object
+class state:
+    """ Jupyter notebook state attached to notebook name.
+    Useful when running programatically and interactively and need to save parameters for next user.
     """
 
-    # initialize hidden state directory
-    if not os.path.exists(statedir): os.mkdir(statedir)
+    def __init__(self, statedir):
+        """ Initialize with directory to save state as files """
 
-    objloc = '{0}/{1}'.format(statedir, label)
+        self.statedir = statedir
 
-    with open(objloc, 'w') as fp:
-        if format == 'pickle':
-            pickle.dump(obj, fp)
-        elif format == 'text':
-            fp.write(str(obj))
+        if not os.path.exists(self.statedir): os.mkdir(self.statedir)
 
 
-def read(label, statedir='.nbpipeline'):
-    """ Read obj with give label from hidden state directory """
+    @property
+    def objects(self):
+        """ List names of stored objects """
+        return os.listdir(self.statedir)
 
-    objloc = '{0}/{1}'.format(statedir, label)
 
-    try:
-        obj = pickle.load(open(objloc, 'r')) 
-    except (KeyError, IndexError, EOFError):
-        obj = open(objloc, 'r').read()
+    def save(self, obj, label, format='text'):
+        """ Save or update obj as pkl file with name label 
+
+        format can be 'text' or 'pickle'.
+        """
+
+        # initialize hidden state directory
+
+        objloc = '{0}/{1}'.format(self.statedir, label)
+
+        with open(objloc, 'w') as fp:
+            if format == 'pickle':
+                pickle.dump(obj, fp)
+            elif format == 'text':
+                fp.write(str(obj))
+
+
+    def load(self, label):
+        """ Load obj with give label from hidden state directory """
+
+        objloc = '{0}/{1}'.format(self.statedir, label)
+
         try:
-            obj = float(obj)
-        except ValueError:
-            pass
-    except IOError:
-        obj = None
+            obj = pickle.load(open(objloc, 'r')) 
+        except (KeyError, IndexError, EOFError):
+            obj = open(objloc, 'r').read()
+            try:
+                obj = float(obj)
+            except ValueError:
+                pass
+        except IOError:
+            obj = None
 
-    return obj
-
-
-def list(statedir = '.nbpipeline'):
-    """ List names of stored objects """
-
-    print(os.listdir(statedir))
+        return obj
 
 
-def setText(label, default='', description='Set Text', format='text', statedir='.nbpipeline'):
-    """ Set text in a notebook pipeline (via interaction or with nbconvert) """
+    def setText(self, label, default='', description='Set Text', format='text'):
+        """ Set text in a notebook pipeline (via interaction or with nbconvert) """
 
-    obj = read(label)
-    if obj == None:
-        obj=default
-        save(obj, label)  # initialize with default
+        obj = self.load(label)
+        if obj == None:
+            obj=default
+            self.save(obj, label)  # initialize with default
 
-    textw = Text(value=obj, description=description)
-    hndl = interact(save, obj=textw, label=fixed(label), format=fixed(format), statedir=fixed(statedir))
-
-
-def setFloat(label, default=0, min=-20, max=20, description='Set Float', format='text', statedir='.nbpipeline'):
-    """ Set float in a notebook pipeline (via interaction or with nbconvert) """
-
-    obj = read(label)
-    if obj == None:
-        obj=default
-        save(obj, label)  # initialize with default
-
-    floatw = FloatSlider(value=obj, min=min, max=max, description=description)
-    hndl = interact(save, obj=floatw, label=fixed(label), format=fixed(format), statedir=fixed(statedir))
+        textw = Text(value=obj, description=description)
+        hndl = interact(self.save, obj=textw, label=fixed(label), format=fixed(format))
 
 
-def setDropdown(label, default=None, options=[], description='Set Dropdown', format='text', statedir='.nbpipeline'):
-    """ Set float in a notebook pipeline (via interaction or with nbconvert) """
+    def setFloat(self, label, default=0, min=-20, max=20, description='Set Float', format='text'):
+        """ Set float in a notebook pipeline (via interaction or with nbconvert) """
 
-    obj = read(label)
-    if obj == None:
-        obj=default
-        save(obj, label)  # initialize with default
+        obj = self.load(label)
+        if obj == None:
+            obj=default
+            self.save(obj, label)  # initialize with default
 
-    dropdownw = Dropdown(value=obj, options=options, description=description)
-    hndl = interact(save, obj=dropdownw, label=fixed(label), format=fixed(format), statedir=fixed(statedir))
+        floatw = FloatSlider(value=obj, min=min, max=max, description=description)
+        hndl = interact(self.save, obj=floatw, label=fixed(label), format=fixed(format))
 
 
-def setButton(description=''):
-    """ Not working... Not clear how onclick function is called... """
+    def setDropdown(self, label, default=None, options=[], description='Set Dropdown', format='text'):
+        """ Set float in a notebook pipeline (via interaction or with nbconvert) """
 
-    button=Button(description=description)
-    out=Output()
-    vbox=VBox(children=(button, out))
-    display(vbox)
+        obj = self.load(label)
+        if obj == None:
+            obj=default
+            self.save(obj, label)  # initialize with default
 
-    def onclick(b):
-        interactive.filterdata(data=data, plinds=plinds, d=d, threshold=nbpipeline.read('threshold'),
-                           ignorestr=nbpipeline.read('ignorestr'), thresh0=6., thresh1=7.)
-
-    button.on_click(onclick)
+        dropdownw = Dropdown(value=obj, options=options, description=description)
+        hndl = interact(self.save, obj=dropdownw, label=fixed(label), format=fixed(format))
 
 
 def getnbname():
