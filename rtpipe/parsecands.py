4b5be310d4f19b98964c058993714b9d1498b0fc
Casey Law <caseyjlaw@gmail.com>
2017-02-28 16:20:23 -0700

    bump version. fixed empty cands file bug

--- rtpipe/parsecands.py
+++ rtpipe/parsecands.py
@@ -56,10 +56,15 @@ def read_candidates(candsfile, snrmin=0, snrmax=999, returnstate=False):
     elif isinstance(cands, tuple):
         loc, prop = cands
         assert isinstance(loc, np.ndarray) and isinstance(prop, np.ndarray), 'if cands object is tuple, contents must be two ndarrays'
-        if len(prop):
-            snrsel = np.where( (np.abs(prop[:, snrcol]) > snrmin) & (np.abs(prop[:, snrcol]) < snrmax) )
-            loc = loc[snrsel]
-            prop = prop[snrsel]
+        if not len(loc):
+            if returnstate:
+                return (loc, prop, d)
+            else:
+                return (loc, prop)
+
+        snrsel = np.where( (np.abs(prop[:, snrcol]) > snrmin) & (np.abs(prop[:, snrcol]) < snrmax) )
+        loc = loc[snrsel]
+        prop = prop[snrsel]
 
     else:
         logger.error('Cands object (in cands file) must be dict or tuple(np.array, np.array).')
